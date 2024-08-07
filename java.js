e0f16778d703a9cd2850856df87a2ae6d80a0dc7
14Berhee <berhee14@gmail.com>
2024-08-07 12:23:08 +0800

    Add files via upload

---
+++ java.js
@@ -0,0 +1,577 @@
+// let miniitoo = 9;
+// let text = "hello";
+// const sol = 44;
+// const summer = "nice";
+
+// console.log("miniitoo");
+// console.log("textuud");
+
+// let eHniitoo = 3;
+// let hoyordahitoo = 4;
+// let guravdahitoo = 5;
+// let p = (eHniitoo, hoyordahitoo, guravdahitoo);
+
+// console.log(eHniitoo + hoyordahitoo + guravdahitoo);
+
+// let ehniitoo = 12;
+// let hoyordahitoo = 23;
+// let p = ehniitoo + hoyordahitoo;
+
+// console.log(ehniitoo + hoyordahitoo);
+
+// let ehniitoo = 5;
+// let hoyordahitoo = 5;
+// let guravdahitoo = 5;
+// let p = ehniitoo * hoyordahitoo * guravdahitoo;
+
+// console.log(ehniitoo * hoyordahitoo * guravdahitoo);
+
+// let ehniitoo = 6;
+// let hoyordahitoo = 4;
+// let p = ehniitoo * hoyordahitoo;
+
+// console.log(ehniitoo * hoyordahitoo);
+
+// let ehniitoo = 3;
+// let hoyordahitoo = 1;
+// let guravdahitoo = 5;
+
+// let p = ehniitoo * hoyordahitoo - guravdahitoo;
+
+// console.log(ehniitoo * hoyordahitoo - guravdahitoo);
+
+// let eHniitoo = 4;
+// let hoyordahitoo = -2;
+// let nemelttoo = 8;
+// let guravdahitoo = 3;
+// let duruvdehtoo = 5;
+
+// let p =
+//   eHniitoo * hoyordahitoo * nemelttoo -
+//   guravdahitoo * hoyordahitoo +
+//   duruvdehtoo;
+
+// console.log(
+//   eHniitoo * hoyordahitoo * nemelttoo -
+//     guravdahitoo * hoyordahitoo +
+//     duruvdehtoo
+// );
+
+// let ehniitoo = 5;
+
+// let p = ehniitoo ** ehniitoo;
+
+// console.log(ehniitoo * 5);
+
+// let ehniitoo = 476;
+// let hoyordahitoo = 100;
+// let guravdahitoo = 2;
+// let duruvdehtoo = 6;
+
+// console.log((ehniitoo % hoyordahitoo) / guravdahitoo / duruvdehtoo);
+
+// let sec = 200;
+// let minut = 60;
+// let p = sec / minut;
+
+// console.log(sec / minut);
+
+// let inputSec = 3612;
+// let sec = inputSec % 60;
+// let hours = 60 / 60;
+// let minute = (inputSec - sec) / 60 / 60;
+// console.log(hours, minute, sec);
+
+// let inputMin = 3 * 60;
+// let inputSec = 4 + inputMin;
+
+// console.log(inputSec);
+
+// let inputTsag = 44;
+// let honog = inputTsag % 24;
+// let tsag = 0;
+
+// let firsNumber = 16;
+// let secondNumber = 14;
+
+// if (firsNumber > secondNumber) {
+//   console.log("firsNumber", firsNumber);
+// } else {
+//   console.log("secondNumber", secondNumber);
+// }
+
+// let firsNumber = 9;
+// let secondNumber = 6;
+
+// if (secondNumber < firsNumber) {
+//   console.log("secondNumber", secondNumber);
+// } else {
+//   console.log("firstNumber", firsNumber);
+// }
+
+// let firsNumber = 3;
+// let secondNumber = 2;
+// let thirdNumber = 1;
+
+// if (firsNumber > secondNumber) {
+//   console.log("firsNumber", firsNumber);
+// } else if (secondNumber > thirdNumber) {
+//   console.log("secondNumber", secondNumber);
+// } else {
+//   console.log("firsNumber", firsNumber);
+// }
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// let d = 4;
+
+// if (c > d) {
+//   console.log("c", c);
+// } else if (b > c) {
+//   console.log("b", b);
+// } else if (a < b) {
+//   console.log("a", a);
+// } else {
+//   console.log("a", a);
+// }
+
+// let ehniiToo = 85;
+// let hoyordahitoo = 75;
+// let guravdahitoo = 96;
+// let duruvdehtoo = 69;
+
+// let sum = 0;
+
+// if (80 < ehniiToo) {
+//   sum = sum + ehniiToo;
+// }
+
+// if (80 < hoyordahitoo) {
+//   sum = sum + hoyordahitoo;
+// }
+// if (80 < guravdahitoo) {
+//   sum = sum + guravdahitoo;
+// }
+// if (80 < duruvdehtoo) {
+//   sum = sum + duruvdehtoo;
+// }
+// console.log(sum);
+
+// let ehniiToo = 3;
+// let hoyordahitoo = 6;
+// let guravdahitoo = 2;
+// let duruvdehtoo = 4;
+
+// let sum = 1;
+
+// if (5 > ehniiToo) {
+//   sum = sum * ehniiToo;
+// }
+// if (5 > hoyordahitoo) {
+//   sum = sum * hoyordahitoo;
+// }
+// if (5 > guravdahitoo) {
+//   sum = sum * guravdahitoo;
+// }
+// if (5 > duruvdehtoo) {
+//   sum = sum * duruvdehtoo;
+// }
+// console.log(sum);
+
+// let a = 10;
+// let b = 3;
+// let c = 5;
+
+// let sum = 0;
+
+// if (a % 2 == 0) {
+//   console.log(a);
+// }
+
+// let ehniiToo = 5;
+// let hoyordahitoo = 2;
+// let guravdahitoo = 3;
+
+// let sum = 1;
+
+// if (2 < ehniiToo) {
+//   sum = sum * ehniiToo;
+// }
+// if (2 < hoyordahitoo) {
+//   sum = sum * hoyordahitoo;
+// }
+// if (2 < guravdahitoo) {
+//   sum = sum * guravdahitoo;
+// }
+// console.log(sum);
+
+// let eHniitoo = 2;
+// let hoyordahitoo = 5;
+// let guravdahitoo = 5;
+
+// let sum = 0;
+
+// if (5 == eHniitoo) {
+//   sum = sum + 1;
+// }
+// if (5 == hoyordahitoo) {
+//   sum = sum + 1;
+// }
+// if (5 == guravdahitoo) {
+//   sum = sum + 1;
+// }
+
+// console.log(sum);
+
+// let ehniiToo = 3;
+// let hoyordahitoo = 12;
+// let guravdahitoo = 8;
+// let duruvdehtoo = 9;
+
+// let sum = 0;
+
+// if (3 % ehniiToo) {
+//   sum = sum + 1;
+// }
+// if (3 % hoyordahitoo) {
+//   sum = sum + 1;
+// }
+// if (3 % guravdahitoo) {
+//   sum = sum + 1;
+// }
+// if (3 % duruvdehtoo) {
+//   sum = sum + 1;
+// }
+// console.log(sum);
+
+// let ehniiToo = 7;
+// let hoyordahitoo = 22;
+// let guravdahitoo = 13;
+// let duruvdehtoo = 30;
+
+// let sum = 0;
+
+// if (ehniiToo % 11 != 0) {
+//   sum = sum + ehniiToo;
+// }
+// if (hoyordahitoo % 11 != 0) {
+//   sum = sum + hoyordahitoo;
+// }
+// if (guravdahitoo % 11 != 0) {
+//   sum = sum + guravdahitoo;
+// }
+// if (duruvdehtoo % 11 != 0) {
+//   sum = sum + duruvdehtoo;
+// }
+// console.log(sum);
+
+// const answer = prompt("asuutaa bich");
+
+// if (answer >= 10) {
+//   console.log("Yes");
+// } else {
+//   console.log("No");
+// }
+
+// const answer = prompt ("Asuultaa bich");
+
+// if (answer<= 5){
+//   console.log("Yes");
+// } else{
+//   console.log("No");
+// }
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// let sumOfValues = sum(1, 2);
+
+// console.log(sumOfValues);
+
+// function sum(a, b, c) {
+//   let result = (a + b + c) / 3;
+//   return result;
+// }
+// let sumOfValues = sum(1, 1, 2);
+
+// console.log(sumOfValues);
+
+// function bagiinDundajOnoo(a, b, c) {
+//   let result = (a + b + c) / 3;
+//   return result;
+// }
+
+// let ehniiBagDundaj = (1, 3, 5);
+// let hoyorDahiBag = (7, 4, 7);
+
+// function winner(aBaginOnoo, bBaginOnoo) {
+//   if (bBaginOnoo * 2 <= aBaginOnoo) {
+//     return "a bag yalsan";
+//   }
+//   if (aBaginOnoo * 2 <= bBaginOnoo) {
+//     return "b bag yalsan";
+//   }
+
+//   return "ali ch bag ylaagui";
+// }
+
+// let result = winner(1, 3);
+
+// console.log(result);
+
+// function taslbariinUne(nas) {
+//   if (nas < 12) {
+//     return "5$";
+//   }
+//   if (nas < 18) {
+//     return "13$";
+//   }
+//   if (nas < 60) {
+//     return "20$";
+//   }
+//   if (nas > 60) {
+//     return "15$";
+//   }
+// }
+// let result = taslbariinUne(71);
+// console.log(result);
+
+// function uniinHunglult(une) {
+//   if (une > 100) {
+//     return "20%";
+//   }
+//   if (une > 50) {
+//     return "10%";
+//   }
+//   if (une > 0) {
+//     return "0%";
+//   }
+// }
+
+// let result = uniinHunglult(60);
+// console.log(result);
+
+// function angiinDun(dun) {
+//   if (dun >= 90) {
+//     return "A";
+//   }
+//   if (dun >= 80) {
+//     return "B";
+//   }
+//   if (dun >= 70) {
+//     return "c";
+//   }
+//   if (dun >= 60) {
+//     return "D";
+//   }
+// }
+// let result = angiinDun(70);
+// console.log(result);
+
+// let engineIndicatoLight = "red blinking";
+// let spaceSuitsOn = true;
+// let shuttleCabinReady = true;
+// let crewStatus = spaceSuitsOn && shuttleCabinReady;
+// let computerStatusCode = 200;
+// let shuttleSpeed = 1500;
+
+// if (engineIndicatoLight == "green") {
+//   console.log("engine started");
+// } else if (engineIndicatoLight == "green blinking") {
+//   console.log("engines are preparing to start");
+// } else {
+//   console.log("engines are off ");
+// }
+
+// if (crewStatus === true) {
+//   console.log("crew ready");
+// } else {
+//   console.log("crew not ready ");
+// }
+
+// if (computerStatusCode === 200) {
+//   console.log("Please stand by Computer online");
+// } else if (computerStatusCode === 400) {
+//   console.log("success computer on");
+// } else {
+//   console.log(" ALsERT: computer is off");
+// }
+
+// if (shuttleSpeed === 17500) {
+//   console.log("ALERT: Escape velocity reached ");
+// } else if (shuttleSpeed === 8000) {
+//   console.log(" ALERT: cannot maintain orbit");
+// } else {
+//   console.log("ALERT: speed is stable");
+// }
+
+// function fuelStatusMonitor(FuelLevel, engineTemp, engineIndicatoLight) {
+//   if (engineIndicatoLight >= 20000 && engineTemp <= 2500) {
+//     console.log("cleared to launch");
+//   }
+//   if (FuelLevel > 20000 && engineTemp <= 2500) {
+//     console.log("fuel tank full , engines are good");
+//   } else if (FuelLevel > 10000 && engineTemp <= 2500) {
+//     console.log("fuel level above 50% , engine are good");
+//   } else if (FuelLevel > 5000 && engineTemp <= 2500) {
+//     console.log(" fuel level 25%, engines  good");
+//   } else if (FuelLevel > 5000 && engineTemp >= 3000) {
+//     console.log("check fuel , engines running hot ");
+//   } else if (FuelLevel <= 1000 && engineTemp >= 3500) {
+//     console.log("engine failure iminent");
+//   } else {
+//     console.log("fuel and engine status are pending");
+//   }
+// }
+// fuelStatusMonitor(21000, 2400);
+
+// let $box = document.getElementById("box");
+// let $addElement = document.getElementById("addElement");
+
+// function createTitleElement() {
+//   let text = "hello";
+//   let elementString = `<h1 id="myP" >${text}</h1>`;
+
+//   $box.innerHTML = elementString;
+// }
+
+// $addElement.onclick = createTitleElement;
+function createCircle() {
+  let circle = document.createElement("div");
+
+  circle.classList.add("Dugui");
+
+  circle.addEventListener("click", function () {
+    circle.style.backgroundColor = "#ffffff";
+  });
+
+  let container = document.getElementById("DuguiContainer");
+  container.appendChild(circle);
+}
+createCircle();
+// modal heseg
+
+let modal = document.getElementById("MyModal");
+let myBtn = document.getElementById("MyBtn");
+let submit = document.getElementById("submitButton");
+
+const modalbuteeh = () => {
+  modal.style.display = "block";
+};
+myBtn.onclick = modalbuteeh;
+
+// sectioniing holboh heseg
+
+// input iin value g awch haruulna
+// shineer element uusgeh createElement
+// shineer vvsgesen element rvvgee innerText ashiglaad input iin value g oruulna
+// shineer vvsgesen element ee todoContainer luugaa innerHTML ashiglaj haruulna
+
+// const taskUusgeh = () => {
+//   let section = document.getElementById("Taskname").value;
+//   let newElement = document.createElement("p");
+//   let zurag = document.createElement("div");
+//   zurag.setAttribute("class", "zurag");
+//   let editImage = document.createElement("img");
+//   editImage.src = "./Vector (1).png";
+//   let status = document.getElementById("statusSelect");
+//   status.setAttribute("id", "statusSelect");
+//   progressContainer.setAttribute("id", "progress");
+//   doneContainer.setAttribute("id", "done");
+//   blockContainer.setAttribute("id", "block");
+
+//   let deleteImage = document.createElement("img");
+//   deleteImage.src = "./Frame.png";
+//   let select = document.getElementById("sectionSelected");
+//   newElement.innerText = section;
+//   zurag.appendChild(editImage);
+//   zurag.appendChild(deleteImage);
+//   let comp = document.createElement("div");
+//   comp.setAttribute("class", "todo-comp");
+//   modal.style.display = "none";
+//   comp.style.color = "white";
+
+//   let Dugui = document.createElement("div");
+//   Dugui.setAttribute("class", "DuguiContainer");
+
+//   Dugui.style.border = " 1px solid white;";
+
+//   let section2 = document.createElement("div");
+//   section2.setAttribute("class", "todo-section2");
+//   comp.appendChild(newElement);
+//   section2.appendChild(Dugui);
+//   section2.appendChild(comp);
+//   section2.appendChild(zurag);
+//   todoContainer.appendChild(section2);
+//   if (status.value == 0) {
+//     todoContainer.appendChild(section2);
+//   }
+//   if (status.value == 1) {
+//     progressContainer.appendChild(section2);
+//   }
+//   if (status.value == 2) {
+//     doneContainer.appendChild(section2);
+//   }
+//   if (status.value == 3) {
+//     blockContainer.appendChild(section2);
+//   }
+// };
+// submit.onclick = taskUusgeh;
+let modalText = document.getElementById("modalText");
+let todoContainer = document.getElementById("todoContainer");
+let progressContainer = document.getElementById("todoProgress");
+let doneContainer = document.getElementById("todoContainer3");
+let blockContainer = document.getElementById("blockContainer");
+
+let tasks = [];
+
+let todoInput = document.getElementsByClassName("todo-input")[0];
+let todoSelect = document.getElementById("statusSelect");
+let todoSubmitButton = document.getElementById("submitButton");
+
+function submitTask() {
+  let taskText = todoInput.value;
+  let todoStatus = todoSelect.value;
+
+  let task = {
+    text: taskText,
+    status: todoStatus,
+  };
+
+  tasks.push(task);
+  renderTasks();
+}
+
+todoSubmitButton.onclick = submitTask;
+
+function renderTasks() {
+  let todoTasksHTML = "";
+  let inprogressTasksHTML = "";
+
+  tasks.forEach((task) => {
+    let _task = createTaskElement(task.text);
+    if (task.status === "todo") {
+      todoTasksHTML = todoTasksHTML + _task;
+    }
+    if (task.status === "inprogress") {
+      inprogressTasksHTML = inprogressTasksHTML + _task;
+    }
+  });
+  todoContainer.innerHTML = todoTasksHTML;
+  progressContainer.innerHTML = inprogressTasksHTML;
+}
+
+function createTaskElement(text) {
+  let _task = `<div class="todo-section2">
+  <div id="Dugui4Container"></div>
+  <div class="todo-comp">
+    <p>${text}</p>
+  </div>
+  <div class="zurag">
+    <img src="./Vector (1).png" />
+    <img src="./Frame.png" />
+  </div>
+</div>`;
+  return _task;
+}
---
+++ loop.js
@@ -0,0 +1,149 @@
+// for (let i = 1; i < 51q; i = i + 1) {
+//   console.log(i);
+// }
+
+// let str = "lorem ipsun";
+
+// let utasniiLength = str.length;
+
+// for (let i = 0; i < utasniiLength; i++) {
+//   let utasniiChar = str[i];
+
+//   console.log(utasniiChar);
+// }
+
+// let languages = [ "java" , " python"  , "css" , "html"];
+
+// for (let i=0 ; i< languages; i++){
+//     let languageChar
+// }
+
+// let str = "lorem";
+// let ugLength = str.length;
+
+// for (let i = 4; -i < ugLength; i--) {
+//   let ugChar = str[i];
+//   console.log(ugChar);
+// }
+
+// let n = 10;
+// let answer = 1;
+
+// while (n > 0) {
+//   answer = answer + n;
+//   n = n - 1;
+// }
+// console.log(answer);
+
+// const boards = document.getElementsByClassName("board");
+
+// for (let i = 0; i <= boards.length; i++) {
+//   let board = boards[i];
+//   let boardClassList = board.classList;
+//   let isTodoBoard = boardClassList.contains("todo");
+
+//   if (isTodoBoard) {
+//     console.log("ene element mun");
+//     console.log("i");
+//     console.log(boards[i]);
+//   }
+// }
+
+// function discount(price) {
+//   let discountedAmount = price * 0.2;
+
+//   let discountedPrice = price - discountedAmount;
+
+//   return discountedPrice;
+// }
+
+// let products = [
+//   {
+//     name: "Smartphone",
+//     price: 599.99,
+//     stock: 50,
+//     category: "Electronics",
+//     brand: "Samsung",
+//   },
+//   {
+//     name: "Laptop",
+//     price: 1099.99,
+//     stock: 30,
+//     category: "Electronics",
+//     brand: "Apple",
+//   },
+//   {
+//     name: "Headphones",
+//     price: 129.99,
+//     stock: 100,
+//     category: "Electronics",
+//     brand: "Sony",
+//   },
+//   {
+//     name: "Running Shoes",
+//     price: 79.99,
+//     stock: 80,
+//     category: "Sports",
+//     brand: "Nike",
+//   },
+//   {
+//     name: "Coffee Maker",
+//     price: 49.99,
+//     stock: 20,
+//     category: "Home Appliances",
+//     brand: "Keurig",
+//   },
+//   {
+//     name: "Backpack",
+//     price: 39.99,
+//     stock: 60,
+//     category: "Fashion",
+//     brand: "Adidas",
+//   },
+//   {
+//     name: "Watch",
+//     price: 199.99,
+//     stock: 40,
+//     category: "Accessories",
+//     brand: "Casio",
+//   },
+//   {
+//     name: "Gaming Console",
+//     price: 399.99,
+//     stock: 25,
+//     category: "Electronics",
+//     brand: "Microsoft",
+//   },
+//   {
+//     name: "Sunglasses",
+//     price: 149.99,
+//     stock: 70,
+//     category: "Fashion",
+//     brand: "Ray-Ban",
+//   },
+//   {
+//     name: "Wireless Speaker",
+//     price: 89.99,
+//     stock: 45,
+//     category: "Electronics",
+//     brand: "JBL",
+//   },
+// ];
+
+// function discountToProducst(price) {
+//   for (let i = 0; i < products.length; i++) {
+//     let product = products[i];
+
+//     if (product.price > price) {
+//       let productPrice = discount(product.price);
+
+//       console.log(productPrice);
+//     }
+//   }
+// }
+
+// discountToProducst(100);
+
+let davharBolonHaalga = [
+  { ehniiDavhar: 5, hoyorDahiDavhar: 4, haalganiiDugaar: 10 },
+];
